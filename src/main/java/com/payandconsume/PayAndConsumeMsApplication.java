37f41de04db0e3f23a207dab4f4b05e17574ec05
trini-git <pablo.david.trinidad.carhuas@everis.com>
2019-12-19 22:34:40 -0500

    PayAndConsumeMS 19122019 22:34

---
+++ src/main/java/com/payandconsume/PayAndConsumeMsApplication.java
@@ -0,0 +1,13 @@
+package com.payandconsume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayAndConsumeMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayAndConsumeMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payandconsume/PayAndConsumeMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payandconsume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayAndConsumeMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
